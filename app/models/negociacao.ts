493bb075dd1b3b457060462b8ad5ed2ba434bff2
Bernardo Mariné <bernardo.marine@villela.local>
2025-11-19 10:24:43 -0300

    criação do tsconfig.json
    configuração do compilador para criar novos arquivos em js na pasta dist, que nós criamos em TS na pasta app.

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,16 +1,39 @@
+//permite exportar 
 export class Negociacao {
-    constructor(
-        private _data: Date, 
-        public readonly quantidade: number, 
-        public readonly valor: number
-    ) {}
+//tralha (#) representa os modificadores de acesso (private, public e protected)
+    #data;
+    #quantidade;
+    #valor;
 
-    get volume(): number {
-        return this.quantidade * this.valor;
+    constructor(data,quantidade,valor) {
+        this.#data = data
+        this.#quantidade = quantidade 
+        this.#valor = valor
     }
 
-    get data(): Date {
-        const data = new Date(this._data.getTime());
-        return data;
+    get data() {
+        return this.#data;
     }
+
+    get quantidade() {
+        return this.#quantidade
+    }
+
+    get valor(){
+        return this.#valor
+
+    }
+
+    // set data(data) {
+    //  return this.#data = data
+    // }
+
+    //  set quantidade(quantidade) {
+    //  return this.#quantidade = quantidade
+    // }
+
+    //  set valor(valor) {
+    //  return this.#valor = valor
+    // }
 }
+ 
