889c5e5d384700e068b2111d5e83ace27f504ed3
Bernardo Mariné <bernardo.marine@villela.local>
2025-12-10 11:45:29 -0300

    programação defensiva no negociacao.ts

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -2,42 +2,23 @@
 export class Negociacao {
 //tralha (#) representa os modificadores de acesso (private, public e protected)
     private _data;
-    private _quantidade;
-    private _valor;
+    public  readonly quantidade
+    public  readonly valor
 
     constructor(data: Date,quantidade: number,valor: number) {
         this._data = data
-        this._quantidade = quantidade 
-        this._valor = valor
+        this.quantidade = quantidade 
+        this.valor = valor
     }
 
-    get getData(): Date {
-        return this._data;
-    }
-
-    get getQuantidade(): number {
-        return this._quantidade
-    }
-
-    get getValor(): number{
-        return this._valor
-
+    get data(): Date {
+        const data = new Date(this._data.getTime())
+        return data;
     }
 
     get getVolume(): number {
-        return this._quantidade * this._valor
+        return this.quantidade * this.valor
     }
 
-    // set data(data) {
-    //   this.#data = data
-    // }
-
-    //  set quantidade(quantidade) {
-    //   this.#quantidade = quantidade
-    // }
-
-    //  set valor(valor) {
-    //   this.#valor = valor
-    // }
 }
  
