35a8dedc035c8e0b39aed692ccd3001ded71ce5d
Bernardo Mariné <bernardo.marine@villela.local>
2025-12-19 12:00:52 -0300

    reorganização de páginas

---
+++ app/src/views/view.ts
@@ -0,0 +1,33 @@
+export abstract class View<T> {
+     protected elemento: HTMLElement;
+     private escapar = false;
+    
+     //torna opicional o parâmetro escapar.
+    constructor(seletor: string, escapar?: boolean) {
+        const elemento = document.querySelector(seletor)
+        //verifica se o nosso elemento exitir garantimos que ele não será nulo forçando que sera um HTMLElement
+        if(elemento) {
+            this.elemento = elemento as HTMLElement
+        } else {
+            throw Error(`Seletor ${seletor} não existe no DOM`)
+        }
+        
+        if(escapar) {
+            this.escapar = escapar
+        }
+    }
+
+     update(model: T): void {
+        const t1 = performance.now()
+        let template = this.template(model)
+        if(this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '')
+        }
+
+        this.elemento.innerHTML = template
+        const t2 = performance.now();
+        console.log(`Tempo de renderização: ${(t2 - t1)/1000} ms`)
+    }
+
+    protected abstract template(model: T): string
+}
