a64623a6fb5714898e351f7e47e1aba9a4777236
bernardo013 <105383046+bernardo013@users.noreply.github.com>
2025-12-23 11:19:44 -0300

    teste cursor

--- app/src/decorators/inspect.ts
+++ app/src/decorators/inspect.ts
@@ -1,20 +1,19 @@
 // não precisa retornar uma função, pois não vai passar parâmetros
-   export function inspect( 
-        target: any,
-        propertyKey: string,  
-        descriptor: PropertyDescriptor  
-    ){ 
-        const metodoOriginal = descriptor.value; 
-        descriptor.value = function(...args: any[])
-        {
-             console.log(`O Método---> ${propertyKey}()`)
-              console.log(`Parâmetro ---> ${JSON.stringify(args)}`)
-             const retorno = metodoOriginal.apply(this, args);
-            console.log(`Retorno ---> ${JSON.stringify(retorno)}`)
-            return retorno;
-        }
-        return descriptor;
+export function inspect(
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+) {
+    const metodoOriginal = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+        console.log(`O Método---> ${propertyKey}()`)
+        console.log(`Parâmetro ---> ${JSON.stringify(args)}`)
+        const retorno = metodoOriginal.apply(this, args);
+        console.log(`Retorno ---> ${JSON.stringify(retorno)}`)
+        return retorno;
     }
-    
+    return descriptor;
+}
+
 
 
