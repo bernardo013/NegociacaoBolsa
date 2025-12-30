0fff80d44ccbba1e4c6666c8bc9d3a810b04dfbb
Bernardo Marques <japatvs1@gmail.com>
2025-12-30 09:40:55 -0300

    explicação do importaDados

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -54,8 +54,12 @@ export class NegociacaoController {
   }
 
 
-  //explicar aqui: 
+// Usamos o fetch para fazer uma requisição HTTP para a URL que retorna os dados.
+//O primeiro .then recebe o objeto Response e converte o corpo da resposta para JSON.
+//O segundo .then tipa esse JSON como um array de NegociacaoDoDia (apenas para o TypeScript) e transforma cada objeto cru em uma instância da classe Negociacao.
+//O último .then percorre o array de negociações criadas e adiciona cada uma na coleção this.negociacoes usando o método adiciona.
   importaDados(): void {
+    //nesse momento tu ainda não tem o JSON, só uma promessa de resposta do servidor.
     fetch("http://localhost:8080/dados")
       .then(res => res.json())
       .then((dados: NegociacaoDoDia[]) => {
@@ -66,6 +70,8 @@ export class NegociacaoController {
       })
       .then(negociacoesDeHoje => { for (let negociacao of negociacoesDeHoje) { this.negociacoes.adiciona(negociacao) } })
   }
+
+
 
   private diaUtil(data: Date) {
     return data.getDay() > DiaDaSemana.DOMINGO && data.getDay() < DiaDaSemana.SABADO
